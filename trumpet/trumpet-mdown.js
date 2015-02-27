b0f7539ceb8132918ca85602833ef97000a519f7
Nick Tomlin <nick.tomlin@gmail.com>
2015-02-26 20:42:38 -0600

    Replace <slide> with <section> and simplify streams a little bit

--- trumpet/trumpet-mdown.js
+++ trumpet/trumpet-mdown.js
@@ -5,21 +5,24 @@ var fs = require('fs');
 var marked = require('marked');
 var es = require('event-stream');
 
+var outfile = fs.createWriteStream('../presentation/output.html');
+// readable/writible stream
 var slides = trumpet();
 var container = trumpet();
 var sliderContainer = container.select('.slides').createStream();
 
-slides.selectAll('slide', function (slide) {
+slides.selectAll('section', function (slide) {
   var stream = slide.createStream();
-
+  // this is wrapping our sections in a <slide>
+  // unfortunately doing the ol' {outer: true} just wraps _every_ element
+  // in <section> instead of just replacing the outer element :(
+  // maybe we just convert to using section? Yes. that's what we did.
+  // we could probably just replace this to split on `---` or something
+  // but this proves a point by being fancy...
   stream.pipe(es.through(function (buff) {
-    this.queue('<section>' + marked(buff.toString()) + '</section>');
+    this.queue(marked(buff.toString()));
   })).pipe(stream);
 });
 
-var pageTemplate = fs.createReadStream('../presentation/index.html').pipe(container);
-var slideTemplate = fs.createReadStream('./source.html').pipe(slides).pipe(es.wait(function (err, data) {
-  sliderContainer.end(data)
-}));
-
-container.pipe(fs.createWriteStream('./output.html'));
+fs.createReadStream('./source.html').pipe(slides).pipe(sliderContainer);
+fs.createReadStream('../presentation/index.html').pipe(container).pipe(outfile);
